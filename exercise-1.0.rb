49e4b5deefaa8be85174000a84e8e63a36455a3c
Esprit Draco <>
2023-01-09 20:28:08 +0000

    Initial commit

---
+++ exercise-1.0.rb
@@ -0,0 +1,43 @@
+#Part 2
+puts 1
+puts "I am a string"
+user = {name: "Greg", age: 42}
+puts user
+array = [4, 9, 10, 12]
+puts array
+puts :symbol
+#Part 3
+puts 1 + 5
+puts 5 - 3
+puts 10 / 2
+puts 20 * 4
+puts 6 ** 3
+puts 39 % 5
+#Part 4
+puts "This is" + " an example of" + " concatenation"
+#Part 5
+alpha = "abcdefghijklmnopqrstuvwxyz"
+puts alpha[6]
+#Part 6
+d = 30
+t = 10
+puts s = d / t
+#Part 7
+has_candy = true
+puts has_candy ? "Yay! Candy" : "*cries*"
+#Part 8
+puts number = 1
+puts 1 == 1
+puts 1 != 2
+puts 4 < 5
+puts 4 > 5
+puts 7 <= 8
+puts 8 >= 8
+#Part 9
+name = "James"
+is_captain = false
+puts "James is Captain" if is_captain == true && name == "James"
+puts "James is present" if is_captain == true || name == "James"
+#Part 10
+age = 5
+puts "I am #{age}" if age < 30
